68c3b380e62256eb87e0bd4a584225a1c11084f3
Antoniu Miclaus <antoniu.miclaus@analog.com>
2021-04-20 14:32:40 +0300

    drivers: adpd410x: add IIO channels
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- drivers/photo-electronic/adpd410x/iio_adpd410x.c
+++ drivers/photo-electronic/adpd410x/iio_adpd410x.c
@@ -45,6 +45,35 @@
 #include "util.h"
 #include "error.h"
 
+/**
+ * @brief Read ADC Channel data.
+ * @param device - Device driver descriptor.
+ * @param buf - Input buffer.
+ * @param len - Length of the input buffer.
+ * @param channel - IIO channel information.
+ * @return Number of bytes printed in the output buffer, or negative error code.
+ */
+static ssize_t adpd410x_iio_channel_read_raw(void *device, char *buf,
+		size_t len,
+		const struct iio_ch_info *channel)
+{
+	struct adpd410x_dev *dev = (struct adpd410x_dev *)device;
+	int32_t ret, data[8];
+
+	ret = adpd410x_set_opmode(dev, ADPD410X_GOMODE);
+	if (ret != SUCCESS)
+		return ret;
+
+	ret = adpd410x_get_data(dev, &data);
+	if (ret != SUCCESS)
+		return ret;
+
+	ret = adpd410x_set_opmode(dev, ADPD410X_STANDBY);
+	if (ret != SUCCESS)
+		return ret;
+
+	return snprintf(buf, len, "%d", data[channel->ch_num]);
+}
 
 /**
  * @brief Set device sampling frequency.
@@ -54,8 +83,9 @@
  * @param channel - IIO channel information.
  * @return Number of bytes printed in the output buffer, or negative error code.
  */
-static ssize_t adpd410x_iio_set_sampling_freq(void *device, char *buf, size_t len,
-				     const struct iio_ch_info *channel, intptr_t priv)
+static ssize_t adpd410x_iio_set_sampling_freq(void *device, char *buf,
+		size_t len,
+		const struct iio_ch_info *channel, intptr_t priv)
 {
 	struct adpd410x_dev *dev = (struct adpd410x_dev *)device;
 	int32_t ret;
@@ -77,8 +107,9 @@ static ssize_t adpd410x_iio_set_sampling_freq(void *device, char *buf, size_t le
  * @param channel - IIO channel information.
  * @return Number of bytes printed in the output buffer, or negative error code.
  */
-static ssize_t adpd410x_iio_set_last_timeslot(void *device, char *buf, size_t len,
-				     const struct iio_ch_info *channel, intptr_t priv)
+static ssize_t adpd410x_iio_set_last_timeslot(void *device, char *buf,
+		size_t len,
+		const struct iio_ch_info *channel, intptr_t priv)
 {
 	struct adpd410x_dev *dev = (struct adpd410x_dev *)device;
 	int32_t ret;
@@ -101,7 +132,7 @@ static ssize_t adpd410x_iio_set_last_timeslot(void *device, char *buf, size_t le
  * @return Number of bytes printed in the output buffer, or negative error code.
  */
 static ssize_t adpd410x_iio_set_opmode(void *device, char *buf, size_t len,
-				     const struct iio_ch_info *channel, intptr_t priv)
+				       const struct iio_ch_info *channel, intptr_t priv)
 {
 	struct adpd410x_dev *dev = (struct adpd410x_dev *)device;
 	int32_t ret;
@@ -115,21 +146,65 @@ static ssize_t adpd410x_iio_set_opmode(void *device, char *buf, size_t len,
 	return len;
 }
 
+/** IIO channel attributes*/
+static struct iio_attribute adpd410x_iio_channel_attributes[] = {
+	{
+		.name = "raw",
+		.show = adpd410x_iio_channel_read_raw,
+		.store = NULL
+	},
+	END_ATTRIBUTES_ARRAY,
+};
+
+/** Channel Scan Type */
+static struct scan_type channel_scan_type = {
+	.sign = 'u',
+	.realbits = 16,
+	.storagebits = 32,
+	.shift = 0,
+	.is_big_endian = false
+};
+
+#define ADPD410X_IIO_CHANN_DEF(nm, ch) \
+	{ \
+		.name = nm, \
+		.ch_type = IIO_VOLTAGE, \
+		.channel = ch, \
+		.scan_type = &channel_scan_type, \
+		.attributes = adpd410x_iio_channel_attributes, \
+		.ch_out = false, \
+		.indexed = 1, \
+		.diferential = false, \
+	}
+
+/** IIO Channels */
+static struct iio_channel adpd410x_iio_channels[] = {
+	ADPD410X_IIO_CHANN_DEF("channel0", 0),
+	ADPD410X_IIO_CHANN_DEF("channel1", 1),
+	ADPD410X_IIO_CHANN_DEF("channel2", 2),
+	ADPD410X_IIO_CHANN_DEF("channel3", 3),
+	ADPD410X_IIO_CHANN_DEF("channel4", 4),
+	ADPD410X_IIO_CHANN_DEF("channel5", 5),
+	ADPD410X_IIO_CHANN_DEF("channel6", 6),
+	ADPD410X_IIO_CHANN_DEF("channel7", 7),
+	END_ATTRIBUTES_ARRAY
+};
+
 /** IIO attributes */
 static struct iio_attribute adpd410x_iio_attributes[] = {
 	{
 		.name = "sampling_frequency",
-		.show = NULL
+		.show = NULL,
 		.store = adpd410x_iio_set_sampling_freq,
 	},
 	{
 		.name = "last_timeslot",
-		.show = NULL
+		.show = NULL,
 		.store = adpd410x_iio_set_last_timeslot,
 	},
 	{
 		.name = "operation_mode",
-		.show = NULL
+		.show = NULL,
 		.store = adpd410x_iio_set_opmode,
 	},
 	END_ATTRIBUTES_ARRAY,
@@ -137,6 +212,8 @@ static struct iio_attribute adpd410x_iio_attributes[] = {
 
 /** IIO Descriptor */
 struct iio_device const adpd410x_iio_descriptor = {
+	.num_ch = 8,
+	.channels = adpd410x_iio_channels,
 	.attributes = adpd410x_iio_attributes,
 	.debug_reg_read = (int32_t (*)())adpd410x_reg_read,
 	.debug_reg_write = (int32_t (*)())adpd410x_reg_write,
